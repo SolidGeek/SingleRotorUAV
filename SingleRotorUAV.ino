e71c590e449fc954411fb144978fa450ae6c6293
Solidgeek <thesolidgeek@gmail.com>
2021-02-16 23:44:12 +0100

    Two DSHOT outputs test

--- SingleRotorUAV.ino
+++ SingleRotorUAV.ino
@@ -1,10 +1,10 @@
 #include "src/dshot.h"
 
-#define DSHOT_N 2
+// Prepare two DSHOT outputs
+#define DSHOT_OUTPUTS 2
+DShot ESC(DSHOT_OUTPUTS); 
 
-DShot ESC(DSHOT_N); // Prepare two DSHOT outputs
-
-uint16_t throttle = 60;
+uint16_t throttle = 200;
 uint8_t tlm = 0;
 
 void setup() {
@@ -12,26 +12,32 @@ void setup() {
   Serial.println("--- DSHOT Teensy 4.0 ---");
 
   Serial1.begin(115200);
-
+  Serial2.begin(115200);
+  
   // Configure the DSHOT outputs
   ESC.setup();
 
   // Get last settings. Cannot be called after settings are changed (dunno why)
-  ESC.requestConfig( 0, &Serial1 );
+  // ESC.requestConfig( 0, &Serial1 );
+  // ESC.requestConfig( 1, &Serial2 );
 
-  ESC.setConfig( 0, DSHOT_CMD_SPIN_DIRECTION_1 );
+  // Config DSHOT ESCs
+  ESC.setConfig( 0, DSHOT_CMD_SPIN_DIRECTION_2 ); 
   ESC.setConfig( 0, DSHOT_CMD_3D_MODE_OFF );
+
+  ESC.setConfig( 1, DSHOT_CMD_SPIN_DIRECTION_1 );
+  ESC.setConfig( 1, DSHOT_CMD_3D_MODE_OFF );
 
   delay(1000);
 
   Serial.println("CAUTION: Arming motors");
-  ESC.armMotor( 0 );
-
+  ESC.armMotors( );
 }
 
 void loop() {
   
   ESC.write(0, throttle, tlm);
+  ESC.write(1, throttle, tlm);
   delay(10);
 
 }
