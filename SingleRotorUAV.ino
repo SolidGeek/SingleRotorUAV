e76d5bf76f282eb94db83a5d5537cce767abfd52
SolidGeek <thesolidgeek@gmail.com>
2021-03-16 15:08:46 +0100

    Motor step test

--- SingleRotorUAV.ino
+++ SingleRotorUAV.ino
@@ -1,7 +1,7 @@
 #include "src/dshot.h"
 
 #define MOTOR_POLES 14    // Number of poles in connected motor
-#define TLM_INTERVAL 500  // Interval in ms
+#define TLM_INTERVAL 5000  // Interval in us
 
 /* Prepare one DSHOT outputs */
 DShot ESC(2); 
@@ -19,12 +19,9 @@ void setup() {
   
   /* Configure the DSHOT outputs */
   ESC.setup();
-
-  /* Retrieve previous settings over TLM wire */
-  ESC.requestConfig( DSHOT_PORT_1, &Serial1 );
-
+  
   /* Config the ESCs */
-  ESC.setConfig( DSHOT_PORT_1, DSHOT_CMD_SPIN_DIRECTION_1 ); 
+  ESC.setConfig( DSHOT_PORT_1, DSHOT_CMD_SPIN_DIRECTION_2 ); 
   ESC.setConfig( DSHOT_PORT_1, DSHOT_CMD_3D_MODE_OFF );
 
   ESC.setConfig( DSHOT_PORT_2, DSHOT_CMD_SPIN_DIRECTION_1 ); 
@@ -34,19 +31,27 @@ void setup() {
   
   /* Arm all DSHOT ports / ESCs */
   ESC.armMotors();
-  Serial.println("DSHOT \t VOLT1 \t AMP1 \t RMP1 \t TEMP1 \t VOLT2 \t AMP2 \t RPM2 \t TEMP2");
 }
 
 uint8_t inputBuffer[5] = {'\0'};
 uint8_t i;
 
-bool newMeasurement = false;
-uint64_t tlmPrintTimer = 0;
+long start_step_timer = 5000;
+int step_measure_time = 5000;
 
 void loop() {
   /* Request telemetry data each TLM_INTERVAL */
-  if( millis() - tlm_timer > TLM_INTERVAL ){
+  if( micros() - tlm_timer > TLM_INTERVAL ){
     tlm = DSHOT_TLM_REQUEST;
+    tlm_timer = micros();
+
+    if( abs(millis() - start_step_timer) < step_measure_time ){
+      Serial.print((String)throttle + "\t");
+      Serial.print((String)millis() + "\t");
+      Serial.print((String)ESC.getRPM( DSHOT_PORT_1, MOTOR_POLES) + "\t");
+      Serial.print((String)ESC.getRPM( DSHOT_PORT_2, MOTOR_POLES) + "\t");
+      Serial.println();
+    }
   }else{
     tlm = DSHOT_TLM_NONE;
   }
@@ -58,22 +63,6 @@ void loop() {
   /* Read DSHOT telemetry from DSHOT_PORT_1 */
   ESC.readTelemetry( DSHOT_PORT_1, &Serial1) ;
   ESC.readTelemetry( DSHOT_PORT_2, &Serial2) ;
-
-
-  if( millis() - tlmPrintTimer > 2000 && newMeasurement == true){
-      Serial.print((String)ESC.getVoltage( DSHOT_PORT_1 ) + "\t");    
-      Serial.print((String)ESC.getAmps( DSHOT_PORT_1 ) + "\t");
-      Serial.print((String)ESC.getRPM( DSHOT_PORT_1, MOTOR_POLES) + "\t");
-      Serial.print((String)ESC.getTemp( DSHOT_PORT_1 ) + "\t");
-      Serial.print((String)ESC.getVoltage( DSHOT_PORT_2 ) + "\t");
-      Serial.print((String)ESC.getAmps( DSHOT_PORT_2 ) + "\t");
-      Serial.print((String)ESC.getRPM( DSHOT_PORT_2, MOTOR_POLES) + "\t");
-      Serial.print((String)ESC.getTemp( DSHOT_PORT_2 ) + "\t");
-      Serial.println();
-
-      newMeasurement = false;
-  }
-
   
   while( Serial.available() ) {
     char c = Serial.read();
@@ -81,16 +70,16 @@ void loop() {
 
     if( c == '\n' ){
       if( strcmp( inputBuffer, "OK\n" ) == 0 ){
-        throttle = throttle + 50;
+        throttle = throttle + 100;
       }else{
         long input = atoi( inputBuffer );
         throttle = input + 47;
-        Serial.println();
+        // Serial.println();
       }
 
-      Serial.print((String)(throttle-47) + "\t");
-      tlmPrintTimer = millis();
-      newMeasurement = true;
+
+      start_step_timer = millis();
+      // Serial.print((String)(throttle-47) + "\t");
       // Serial.println( "Throttle " + (String)(throttle-47) );
 
       i = 0;
