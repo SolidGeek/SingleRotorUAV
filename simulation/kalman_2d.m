8e00a30121269b686c9157e005dec11881887a72
Solidgeek <thesolidgeek@gmail.com>
2021-05-01 01:35:21 +0200

    Kalman-Filter Tuning using measurements

--- simulation/kalman_2d.m
+++ simulation/kalman_2d.m
@@ -1,18 +1,35 @@
 % First run record.m, to sample data from UDP
 % Next run this 
 
-% load('walk_test_2_square_kalman.mat')
+load('walking_test_with_vicon_data.mat')
 
 
 %% Data Import
-z = [data.z];
-% 
-vx = [data.vx]*0.5;
-vy = [data.vy]*0.5;
+x_vicon = [data.xe];
+y_vicon = [data.ye];
+z_vicon = [data.ze];
 
+x_vicon = x_vicon - x_vicon(1);
+y_vicon = y_vicon - y_vicon(1);
+z_vicon = z_vicon - z_vicon(1);
+
+
+x_est = [data.xt];
+y_est = [data.yt];
+z_est = [data.zt];
+
+% Measured lidar data
+z = [data.z];
+
+% Measured flow data (rotated with yaw)
+vx = [data.vx];
+vy = [data.vy];
+
+% Measure acceleration (rotated to body frame)
 ax = [data.ax];
 ay = [data.ay];
 az = [data.az];
+
 
 %% Kalman filter design parameters
 
@@ -20,7 +37,7 @@ az = [data.az];
 % x = [x y z vx vy vz]
 
 % Sample time
-dt = 0.01; 
+dt = 0.005; 
 
 % System Matrix
 A = [ 1  0  0  dt 0  0  ;  % dx
@@ -49,7 +66,7 @@ C = [ 1 0 0 0 0 0  ; % x
 % Calculate discrete steady-state kalman gain
 
 % Process Noice Covariance
-sigma_acc = 0.01; % Variance of acceleration m/s^2
+sigma_acc = 0.25; % Variance of acceleration m/s^2
 % Q = [dt^4/4   0      0        dt^3/2 ;
 %      0        dt^2   0        0      ;
 %      0        0      dt^2     0      ;
@@ -63,15 +80,22 @@ Q = [dt^4/4  0       0       dt^3/2  0       0      ;
      0       0       dt^3/2  0       0       dt^2   ] * sigma_acc;
 
 % Measurement Noise Covariance
-R = [1      0      0        0        0        0 ;
-     0      1      0        0        0        0 ;
-     0      0      5*10^-7  0        0        0 ; % Lidar
-     0      0      0        8*10^-5  0        0 ; % Flow x
-     0      0      0        0        8*10^-5  0 ; % Flow y
+% R = [10^-5  0      0        0        0        0 ;
+%      0      10^-5  0        0        0        0 ;
+%      0      0      10^-6    0        0        0 ; % Lidar
+%      0      0      0        2*10^-3  0        0 ; % Flow x
+%      0      0      0        0        2*10^-3  0 ; % Flow y
+%      0      0      0        0        0        1 ];
+
+R = [0.0001  0      0        0        0        0 ;
+     0      0.0001  0        0        0        0 ;
+     0      0      0.001    0        0        0 ; % Lidar
+     0      0      0        0.01     0        0 ; % Flow x
+     0      0      0        0        0.01     0 ; % Flow y
      0      0      0        0        0        1 ];
 
 G = eye(6)*1;
-kf = dlqe(A,G,C,Q,R)
+kf = dlqe(A,G,C,Q,R);
 
 %% Run Kalman Filter
 
@@ -83,21 +107,30 @@ x(1,:) = [ 0; 0; z(1); 0; 0; 0 ];
 for i = 2:n
     
     u = [ ax(i); ay(i); az(i)];
+    %     u = [0; 0; 0];
     y = [ 0; 0; 0 ; 0; 0; 0 ];
     
     H = zeros(6,6);
 
-    if( data(i).status_lidar == 1 )
+    % Vicon at 10 Hz
+%     if( mod(i,20) == 0 )
+%         H(1:2, 1:2) = eye(2);
+%         y(1) = x_vicon(i);
+%         y(2) = y_vicon(i);
+%         disp('Vicon Data');
+%     end
+
+    if( data(i).stat_lidar == 1 )
         H(3,3) = 1;
         y(3) = z(i);
     end
     
-    if( data(i).status_flow == 1 )
+    if( data(i).stat_flow == 1 )
         H(4:5, 4:5) = eye(2);
         y(4) = vx(i);
         y(5) = vy(i);
     end
-    
+%     
     xpre = A*x(i-1,:)' + B*u;             % Predicted state estimate
     x(i,:) = xpre + kf*(y - H*xpre);   % Updated state estimate
 end
@@ -109,7 +142,6 @@ subplot(2,2,1)
 hold on
 plot( z , 'LineWidth', 2);
 plot( x(:,3), '.' );
-plot( [data.ze] );
 hold off
 grid on
 title("Altitude (z-axis)");
@@ -118,7 +150,6 @@ legend("Measured", "Kalman");
 subplot(2,2,2)
 hold on
 plot( x(:,6) );
-plot( [data.vze] );
 hold off
 grid on
 title("Altitude velocity (z-axis)");
@@ -143,10 +174,20 @@ title("Velocity (y-axis)");
 legend("Flow", "Kalman");
 
 figure(2)
-plot3(x(:,1), x(:,2), x(:,3));
+hold on
+plot3( x_vicon, y_vicon, z_vicon );
+plot3( x(:,1), x(:,2), x(:,3) );
+% plot3( x_est, y_est, z_est );
 title("Position (world)");
-legend("Kalman");
+legend("Vicon", "Kalman", "Estimator");
 xlabel("x [m]");
 ylabel("y [m]");
 axis equal
 grid on
+
+figure(3)
+
+hold on
+plot( x_vicon );
+plot( x(:,1) );
+hold off
